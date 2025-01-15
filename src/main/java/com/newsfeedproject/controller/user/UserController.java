7726a7c0ad785ca97d466fc523b1874993df280a
YJ-Kkang <kyujin995@gmail.com>
2025-01-15 11:51:26 +0900

    feat: User 초기 세팅

---
+++ src/main/java/com/newsfeedproject/controller/user/UserController.java
@@ -0,0 +1,13 @@
+package com.newsfeedproject.controller.user;
+
+import com.newsfeedproject.service.user.UserService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
