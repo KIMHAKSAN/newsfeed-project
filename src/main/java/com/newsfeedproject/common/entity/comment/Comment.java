f2662d530f8d5b33b8fda0f3c2ad58a8b860285d
answerin1 <dabin990723@naver.com>
2025-01-15 22:52:40 +0900

    feat: commentEntity 기능 구현 추가

--- src/main/java/com/newsfeedproject/common/entity/comment/Comment.java
+++ src/main/java/com/newsfeedproject/common/entity/comment/Comment.java
@@ -1,4 +1,55 @@
 package com.newsfeedproject.common.entity.comment;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.newsfeedproject.common.entity.user.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Getter
+@Table(name = "Comment")
 public class Comment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne
+	@JoinColumn(name = "post_id")
+	private Comment parent;
+
+	@JoinColumn(name = "contnet")
+	private String content;
+
+	@CreatedDate
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(name = "updated_at", nullable = false)
+	private LocalDateTime updatedAt;
+
+	// 삭제에 대한 어노테이션이 필요한가?
+	@Column(name = "deleted_at", nullable = false)
+	private LocalDateTime deletedAt;
+
 }
