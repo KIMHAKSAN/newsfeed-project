1ab6af05ba9db5b3ad5f81ea3bf311d80f279291
YJ-Kkang <kyujin995@gmail.com>
2025-01-16 13:03:50 +0900

    style: User 엔티티 주석 변경

--- src/main/java/com/newsfeedproject/common/entity/user/User.java
+++ src/main/java/com/newsfeedproject/common/entity/user/User.java
@@ -13,13 +13,7 @@ import org.hibernate.annotations.Comment;
 public class User {
 
     /**
-     *        bigint user_id PK "유저 식별자"
-     *        VARCHAR(20) user_name "유저명"
-     *        VARCHAR(50) email "이메일"
-     *        VARCHR(255) password "비밀번호"
-     *
-     *
-     *        < todo 이하 BaseEntity에서 상속받을 요소들 >
+     *        < 이하 BaseEntity에서 상속받을 요소들 >
      *        datetime created_at "가입일" ==  "생성일"
      *        datetime upated_at "수정일"
      *        datetime deleted_at "탈퇴일" == "삭제일"
