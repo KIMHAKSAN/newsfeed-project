6380695e6f2d5edcf00ab4b09fe8845be826cc21
KIMHAKSAN <bk351313@naver.com>
2025-01-16 20:57:12 +0900

    friend[feat : 친구 신청 거절, 친구 삭제] 친구를 신청을 거절하고 친구를 삭제 할 수 있게 했습니다.

--- src/main/java/com/newsfeedproject/controller/friend/FriendController.java
+++ src/main/java/com/newsfeedproject/controller/friend/FriendController.java
@@ -2,6 +2,7 @@ package com.newsfeedproject.controller.friend;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,9 +33,25 @@ public class FriendController {
 	}
 
 	//친구 수락
-	@PatchMapping("/{toUserId}/{fromUserId}")  // friends 부분을 제거한 경로
-	public ResponseEntity<String> changeFriendStatus(@PathVariable Long fromUserId, @PathVariable Long toUserId) {
-		friendService.changeFriendStatusService(fromUserId, toUserId);
+	@PatchMapping("/accept/{toUserId}/{fromUserId}")  // friends 부분을 제거한 경로
+	public ResponseEntity<String> AcceptFriendStatus(@PathVariable Long fromUserId, @PathVariable Long toUserId) {
+		friendService.AcceptFriendStatusService(fromUserId, toUserId);
 		return ResponseEntity.ok("Friend status updated successfully");
 	}
+
+	// 친구 거절
+	@PatchMapping("/decline/{toUserId}/{fromUserId}")  // friends 부분을 제거한 경로
+	public ResponseEntity<String> DeclineFriendStatus(@PathVariable Long fromUserId, @PathVariable Long toUserId) {
+		friendService.DeclineFriendStatusService(fromUserId, toUserId);
+		return ResponseEntity.ok("Friend status updated successfully");
+	}
+
+	// 친구 삭제
+	@DeleteMapping("/{id}")
+	public ResponseEntity<FriendResponseDto> delete(@PathVariable Long id) {
+		friendService.deleteFriend(id);
+
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
 }
