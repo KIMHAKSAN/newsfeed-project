492f6c7000386890862330dea88f2a12aa9f22d5
YJ-Kkang <kyujin995@gmail.com>
2025-01-19 00:39:15 +0900

    feat: @Repository 삭제

--- src/main/java/com/newsfeedproject/repository/user/UserRepository.java
+++ src/main/java/com/newsfeedproject/repository/user/UserRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 
 import com.newsfeedproject.common.entity.user.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	/*
